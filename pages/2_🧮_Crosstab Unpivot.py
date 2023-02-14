8be9c03bcb837174d7d090576c512f4793463cf0
Tran Hai Ha <ha.tran@vn.corp.seagroup.com>
2023-02-14 19:28:47 +0700

    remove bokeh

--- pages/2_🧮_Crosstab Unpivot.py
+++ pages/2_🧮_Crosstab Unpivot.py
@@ -2,10 +2,6 @@ from io import StringIO
 import streamlit as st
 import pandas as pd
 
-from bokeh.models.widgets import Button
-from bokeh.models import CustomJS
-from streamlit_bokeh_events import streamlit_bokeh_events
-
 
 st.set_page_config(
     page_title="Crosstab Unpivot",
