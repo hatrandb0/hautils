e92f98fc3ad646b08525668dd8313c92f1bd4f3c
Ha Tran <ha.tran@shopee.com>
2023-08-15 11:15:41 +0700

    add new module

---
+++ pages/1_🌐_SQL Translation.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import sqlglot
+
+st.set_page_config(
+    page_title="🌐 SQL Translation Tool",
+    page_icon="🌐",
+)
+
+st.write("# SQL Translation Tool 🌐")
+
+dialects = {
+    "Trino": "trino",
+    "Spark": "spark",
+}
+
+with st.form(key='input_form'):
+    origin = st.selectbox(
+        "Choose original sql dialect:",
+        dialects.keys()
+            )
+    destination = st.selectbox("Choose dialect to translate to:", dialects.keys())
+
+    origin_query = st.text_area("Enter query to translate:", height=300)
+    
+    to_translate = st.form_submit_button(label='Translate')
+    if to_translate:
+        dest_query = sqlglot.transpile(origin_query, read=dialects[origin], write=dialects[destination], pretty=True)[0]
+        st.divider()
+        st.write("## Translated Query:")
+        st.code(dest_query, language='sql', line_numbers=True)
